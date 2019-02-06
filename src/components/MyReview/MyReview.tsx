06a41c33ab5ef1f6dbd58da73bb04b824473f3cc
Raul Macarie <raul.macarie@gmail.com>
2019-02-06 09:10:28 +0200

    Add structure for my review component

--- src/components/MyReview/MyReview.tsx
+++ src/components/MyReview/MyReview.tsx
@@ -1,12 +1,45 @@
 import React from 'react';
+import moment from "moment";
 
 interface IProps {
+    reviewerPhoto?: string;
+    reviewerName?: string;
+    reviewStars?: number;
+    reviewTime?: number;
+    reviewContent?: string;
 }
 
-const MyReview = (props: IProps) => (
-    <div>
-        My Review
-    </div>
-);
+const MyReview = (props: IProps) => {
+    const noReview = () => (
+        <div>
+            <h3>Rate and review</h3>
+            <span>Share your experience to help others</span>
+            <div>stars</div>
+        </div>
+    );
+
+    const review = () => (
+        <div>
+            <div>{props.reviewerName}</div>
+            <div>
+                <div>
+                    {props.reviewStars}
+                </div>
+                <div>
+                    {moment(props.reviewTime).fromNow()} - hitta.se
+                </div>
+            </div>
+            <div>
+                {props.reviewContent ? props.reviewContent : <span>Describe your experience</span>}
+            </div>
+        </div>
+    );
+    return (
+        <div>
+            <div>{props.reviewerPhoto}</div>
+            {props.reviewStars ? review() : noReview()}
+        </div>
+    );
+};
 
 export default MyReview;
